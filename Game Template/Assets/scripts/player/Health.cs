c622d0ca5ce970ada237bca3cafacd6eb0677250
MUKILTEO\1726098 <uberfrogguy@gmail.com>
2025-09-23 10:08:20 -0700

    ADDED HEALTH BAR DOESNT WORK

--- Game Template/Assets/scripts/player/Health.cs
+++ Game Template/Assets/scripts/player/Health.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public int health;
+    public Text healthText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,6 +15,7 @@ public class Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        healthText.text = health.ToString();
         if(health <= 0)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
