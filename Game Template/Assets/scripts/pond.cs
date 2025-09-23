8a1b7961a39d93bdd628c123549ea5eabdce608e
MUKILTEO\1726098 <uberfrogguy@gmail.com>
2025-09-23 09:35:50 -0700

    just to be sure

--- Game Template/Assets/scripts/pond.cs
+++ Game Template/Assets/scripts/pond.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class pond : MonoBehaviour
 {
   
-    void OnCollisionExit2D(Collision2D collision)
+    void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
